17e54a1c07f53f00fe792796b3492120d3b8681b
Suzanne <suzanne.aitchison@gmail.com>
2017-03-16 20:27:31 +0000

    some basic styling of theme and MainActivity

---
+++ WorkoutPlannerApp/app/src/main/java/com/codeclan/workoutplannerapp/WorkoutListAdapter.java
@@ -0,0 +1,10 @@
+package com.codeclan.workoutplannerapp;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by user on 16/03/2017.
+ */
+
+public class WorkoutListAdapter extends ArrayAdapter<Workout> {
+}
---
+++ WorkoutPlannerApp/app/src/main/java/com/codeclan/workoutplannerapp/WorkoutLog.java
@@ -0,0 +1,8 @@
+package com.codeclan.workoutplannerapp;
+
+/**
+ * Created by user on 16/03/2017.
+ */
+
+public class WorkoutLog {
+}

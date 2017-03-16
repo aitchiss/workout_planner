918ac45807c542f478fab4135d5585a3f7af2f7e
Suzanne <suzanne.aitchison@gmail.com>
2017-03-16 21:03:40 +0000

    workoutlist adapter working

--- WorkoutPlannerApp/app/src/main/java/com/codeclan/workoutplannerapp/MainActivity.java
+++ WorkoutPlannerApp/app/src/main/java/com/codeclan/workoutplannerapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.codeclan.workoutplannerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //do I do something here to recall persisted saved data, and then add it back into a
+        //workout log? Would need to all be handled in WorkoutLog class.
+        //e.g. restoreWorkoutTemplates, restoreCompletedWorkouts.
+
+        WorkoutLog workoutLog = new WorkoutLog();
+        //below just to test adapter/display
+        Workout workout = new Workout("test workout");
+        workout.addMultipleSets(Activity.LUNGES, 10, 25, 3);
+        workoutLog.addWorkoutTemplate(workout);
+        ArrayList<Workout> list = workoutLog.getAllWorkoutTemplates();
+
+        WorkoutListAdapter workoutListAdapter = new WorkoutListAdapter(this, list);
+        ListView listView = (ListView) findViewById(R.id.all_workouts_list);
+        listView.setAdapter(workoutListAdapter);
     }
 }
--- WorkoutPlannerApp/app/src/main/java/com/codeclan/workoutplannerapp/WorkoutListAdapter.java
+++ WorkoutPlannerApp/app/src/main/java/com/codeclan/workoutplannerapp/WorkoutListAdapter.java
@@ -1,10 +1,41 @@
 package com.codeclan.workoutplannerapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 /**
  * Created by user on 16/03/2017.
  */
 
-//public class WorkoutListAdapter extends ArrayAdapter<Workout> {
-//}
+public class WorkoutListAdapter extends ArrayAdapter<Workout> {
+
+    public WorkoutListAdapter(Context context, ArrayList<Workout> workouts){
+        super(context, 0, workouts);
+    }
+
+    @Override
+    public View getView(int position, View listItemView, ViewGroup parent){
+
+        if (listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.workout_list_item, parent,false);
+        }
+
+        Workout currentWorkout = getItem(position);
+
+        TextView workoutName = (TextView) listItemView.findViewById(R.id.workout_name);
+        workoutName.setText(currentWorkout.getName());
+
+//        TextView lastCompletedDate = ??
+
+        return listItemView;
+    }
+
+}
